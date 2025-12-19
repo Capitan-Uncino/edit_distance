eb27b81cbe293788570af0ef07673c1f347005f7
irenerusconi <rusconiirene4@gmail.com>
2025-12-19 01:34:11 +0100

    aggiunto il test sul file

--- edit_distance.jl
+++ edit_distance.jl
@@ -5,144 +5,134 @@ filepath = joinpath(dir, "keyboard_layout_distance.txt")
 filepath2 = joinpath(dir, "words_alpha.txt")
 
 function create_dictionary(filepath)
-  lines = readlines(filepath)
-  data = [split(line) for line in lines]
+    lines = readlines(filepath)
+    data = [split(line) for line in lines]
 
-  D = Dict{Tuple{String,String},Float64}()
+    D = Dict{Tuple{String,String},Float64}()
 
-  for t in data
-    key = (t[1], t[2])
-    value = parse(Float64, t[3])
-    D[key] = value
-  end
+    for t in data
+        key = (t[1], t[2])
+        D[key] = parse(Float64, t[3])
+    end
 
-  return D
+    return D
 end
 
 function normalize_dictionary!(D)
-  max_val, max_key = findmax(D)
-  println(max_val)
-  for k in keys(D)
-    D[k] = D[k] / max_val
-  end
-  return D
+    max_val, max_key = findmax(D)
+    println(max_val)
+    for k in keys(D)
+        D[k] = D[k] / max_val
+    end
+    return D
 end
 
 
 function find_distance(letter_up, letter_left, number_up, number_left, number_upleft)
-  if letter_up == letter_left
-    return number_upleft
-    #return min(number_up,number_left,number_upleft)
-  else
-    return 1 + min(number_up, number_left, number_upleft)
-  end
+    if letter_up == letter_left
+        return number_upleft
+    else
+        return 1 + min(number_up, number_left, number_upleft)
+    end
 end
 
 
 function uninformed_edit_distance(word1, word2)
-  word_up = word1
-  word_left = word2
-  n = length(word_up)
-  m = length(word_left)
-  distances = zeros(Int, m + 1, n + 1)
-  for i in 1:m+1
-    distances[i, 1] = i - 1
-  end
-  for i in 1:n+1
-    distances[1, i] = i - 1
-  end
-
-  for i in 2:m+1
-    for j in 2:n+1
-      distances[i, j] = find_distance(word_up[j-1], word_left[i-1], distances[i-1, j], distances[i, j-1], distances[i-1, j-1])
+    word_up = word1
+    word_left = word2
+    n = length(word_up)
+    m = length(word_left)
+    distances = zeros(Int, m + 1, n + 1)
+    for i in 1:m+1
+        distances[i, 1] = i - 1
     end
-  end
-  return distances[m+1, n+1]
+    for i in 1:n+1
+        distances[1, i] = i - 1
+    end
+
+    for i in 2:m+1
+        for j in 2:n+1
+            distances[i, j] = find_distance(word_up[j-1], word_left[i-1], distances[i-1, j], distances[i, j-1], distances[i-1, j-1])
+        end
+    end
+    return distances[m+1, n+1]
 end
 
 
 function initialize_swaps_distribution_1(distances_path)
+    lines = readlines(distances_path)
 
+    # store as: dict[from_letter] = Dict(to_letter => distance)
+    dist = Dict{Char,Dict{Char,Float64}}()
 
-  lines = readlines(distances_path)
+    for line in lines
+        a, b, d = split(line)
+        d = parse(Float64, d)
 
-  # store as: dict[from_letter] = Dict(to_letter => distance)
-  dist = Dict{Char,Dict{Char,Float64}}()
+        from = a[1]
+        to = b[1]
 
-  for line in lines
-    a, b, d = split(line)
-    d = parse(Float64, d)
-
-    from = a[1]
-    to = b[1]
-
-    get!(dist, from, Dict{Char,Float64}())[to] = d
-  end
-
-
-  weights = Dict{Char,Dict{Char,Float64}}()
-
-  for (from, targets) in dist
-    w = Dict{Char,Float64}()
-    for (to, d) in targets
-      w[to] = 1 / (d + 1)
+        get!(dist, from, Dict{Char,Float64}())[to] = d
     end
-    weights[from] = w
-  end
 
-  probs = Dict{Char,Dict{Char,Float64}}()
 
-  for (from, w) in weights
-    Z = sum(values(w))
-    probs[from] = Dict(to => wt / Z for (to, wt) in w)
-  end
+    weights = Dict{Char,Dict{Char,Float64}}()
 
-  return probs
+    for (from, targets) in dist
+        w = Dict{Char,Float64}()
+        for (to, d) in targets
+            w[to] = 1 / (d + 1)
+        end
+        weights[from] = w
+    end
+
+    probs = Dict{Char,Dict{Char,Float64}}()
+
+    for (from, w) in weights
+        Z = sum(values(w))
+        probs[from] = Dict(to => wt / Z for (to, wt) in w)
+    end
+
+    return probs
 end
 
 
 
 function initialize_swaps_distribution_2(distances_path)
+    lines = readlines(distances_path)
 
+    # store as: dict[from_letter] = Dict(to_letter => distance)
+    dist = Dict{Char,Dict{Char,Float64}}()
 
-  lines = readlines(distances_path)
+    for line in lines
+        a, b, d = split(line)
+        d = parse(Float64, d)
 
-  # store as: dict[from_letter] = Dict(to_letter => distance)
-  dist = Dict{Char,Dict{Char,Float64}}()
+        from = a[1]
+        to = b[1]
 
-  for line in lines
-    a, b, d = split(line)
-    d = parse(Float64, d)
-
-    from = a[1]
-    to = b[1]
-
-    get!(dist, from, Dict{Char,Float64}())[to] = d
-  end
-
-
-  weights = Dict{Char,Dict{Char,Float64}}()
-
-
-  τ = 30.0
-
-  for (from, targets) in dist
-    w = Dict{Char,Float64}()
-    for (to, d) in targets
-      w[to] = exp(-d / τ)
+        get!(dist, from, Dict{Char,Float64}())[to] = d
     end
-    weights[from] = w
-  end
 
+    weights = Dict{Char,Dict{Char,Float64}}()
 
-  probs = Dict{Char,Dict{Char,Float64}}()
+    τ = 30.0
 
-  for (from, w) in weights
-    Z = sum(values(w))
-    probs[from] = Dict(to => wt / Z for (to, wt) in w)
-  end
+    for (from, targets) in dist
+        w = Dict{Char,Float64}()
+        for (to, d) in targets
+            w[to] = exp(-d / τ)
+        end
+        weights[from] = w
+    end
 
-  return probs
+    probs = Dict{Char,Dict{Char,Float64}}()
+
+    for (from, w) in weights
+        Z = sum(values(w))
+        probs[from] = Dict(to => wt / Z for (to, wt) in w)
+    end
+    return probs
 end
 
 
@@ -156,29 +146,72 @@ function sample_swap(probs, from::Char)
 end
 
 
-function apply_swaps(strings::Vector{String},
-  probs::Dict{Char,Dict{Char,Float64}})
-  out = Vector{String}(undef, length(strings))
+function apply_errors(strings::Vector{String},
+    probs::Dict{Char,Dict{Char,Float64}})
 
-  for (i, s) in enumerate(strings)
-    chars = collect(s)
+    out = Vector{String}(undef, length(strings))
 
-    for j in eachindex(chars)
-      c = chars[j]
+    prob_swap   = 0.15
+    prob_insert = 0.05
+    prob_delete = 0.05
+    prob_error   = prob_swap + prob_insert + prob_delete
 
-      # only swap letters we have probabilities for
-      if haskey(probs, c)
-        letters = collect(keys(probs[c]))
-        p = [probs[c][l] for l in letters]
-        chars[j] = letters[rand(Categorical(p))]
-      end
+    for (i, s) in enumerate(strings)
+        chars = collect(s)
+        j = 1
+
+        while j ≤ length(chars)
+            c = chars[j]
+            error_type = rand(Categorical([prob_swap, prob_insert, prob_delete, 1 - prob_error]))
+
+            if error_type == 1   # swap
+                if haskey(probs, c)
+                    letters = collect(keys(probs[c]))
+                    p = [probs[c][l] for l in letters]
+                    chars[j] = letters[rand(Categorical(p))]
+                end
+                j += 1
+
+            elseif error_type == 2   # insert
+                letters = collect(keys(probs[c]))
+                p = [probs[c][l] for l in letters]
+                insert!(chars, j, letters[rand(Categorical(p))])
+                j += 2 
+
+            elseif error_type == 3   # delete
+                deleteat!(chars, j)
+
+            else   # no error
+                j += 1
+            end
+        end
+
+        out[i] = String(chars)
     end
 
-    out[i] = String(chars)
-  end
-
-  return out
+    return out
 end
+
+function find_closest_word(words_correct, words_err)
+    closest_words = Dict{String,String}()
+    closest = ""
+
+    for s_err in words_err
+        d_min = Inf
+        for s_corr in words_correct
+            d = uninformed_edit_distance(s_corr, s_err)
+            if d < d_min
+                d_min = d
+                closest = s_corr
+            end
+        end
+
+        closest_words[s_err] = closest
+    end
+
+    return closest_words
+end
+
 
 
 s = "misspelling"
@@ -189,12 +222,37 @@ uninformed_edit_distance(s, t)
 D = create_dictionary(filepath)
 D = normalize_dictionary!(D)
 
-#words = readlines(filepath2)
+words = readlines(filepath2)
+words = words[1:10000]
 
 
 probs = initialize_swaps_distribution_1(filepath)
-words = ["pizza", "pasta", "mandolino"]
-words_sbagliate = apply_swaps(words, probs)
+#words = ["pizza", "pasta", "mandolino", "misspelling"]
+words = uppercase.(words)
+words_sbagliate = apply_errors(words, probs)
 
-println(words)
-println(words_sbagliate)
+#println(words)
+#println(words_sbagliate)
+
+closest = find_closest_word(words, words_sbagliate)
+
+
+num_correct = 0
+num_incorrect = 0
+
+println("Original     Wrong       Correct    Distance")
+
+for (original, error) in zip(words, words_sbagliate)
+    correct = closest[error]
+    #println("$original\t→     \t$error\t→   \t$correct")
+    
+    if original == correct
+        num_correct += 1
+    else
+        num_incorrect += 1
+    end
+end
+
+println("\nNumero parole corrette: ", num_correct)
+println("Numero parole non corrette: ", num_incorrect)
+println("Accuracy: ", round(num_correct / length(words) * 100, digits=2), "%")
